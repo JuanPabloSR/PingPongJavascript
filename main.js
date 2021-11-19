70296bc5fbcec43bd5e135abfeadc6cf9c047831
Juan Pablo Sánchez Rey <juanpablosanchez744@gmail.com>
2021-11-19 03:25:07 -0500

    v2.0.0 se agrego y dibujo la pelota

--- main.js
+++ main.js
@@ -46,7 +46,20 @@
   };
 })();
 //VISTA BOARDVIEW
+(function(){
+  self.Ball = function(x,y,radius,board){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
 
+    this.speed_y = 0;
+    this.speed_x = 3;
+    this.board = board;
+
+    board.ball = this;
+    this.kind = "circle";
+  }
+}());
 (function () {
   self.BoardView = function (canvas, board) {
     // con el self modificamos el canvas
@@ -58,23 +71,36 @@
   };
 
   self.BoardView.prototype = {
+    clean: function(){
+      this.ctx.clearRect(0,0,this.board.width, this.board.height);
+    },
     draw: function () {
       for (var i = this.board.elements.length - 1; i >= 0; i--) {
         var el = this.board.elements[i];
 
         draw(this.ctx,el);
-      }
+      };
     },
+    play: function(){
+      this.clean();
+      this.draw();
+
+    }
   };
 
   function draw(ctx, element) {
-    if(element !== null && element.hasOwnProperty("kind")) {
       switch (element.kind) {
         case "rectangule":
           ctx.fillRect(element.x, element.y, element.width, element.height);
           break;
+        case "circle":
+        ctx.beginPath();
+        ctx.arc(element.x, element.y, element.radius, 0, 7);
+        ctx.fill();
+        ctx.closePath();
+        break;
       }
-    }
+    
    
   }
 })();
@@ -82,26 +108,38 @@
 
   var board = new Board(800, 400);
   var bar = new Bar(20, 100, 40, 100,board);
-  var bar = new Bar(735, 100, 40, 100,board);
+  var bar_2 = new Bar(735, 100, 40, 100,board);
   var canvas = document.getElementById("canvas");
   var board_view = new BoardView(canvas,board);
+  var ball = new Ball(350, 100, 10, board)
+
 
 document.addEventListener("keydown", function(ev){
+  ev.preventDefault();
   if(ev.keyCode == 38){
     bar.up();
   }
   else if(ev.keyCode == 40){
     bar.down();
   }
-  console.log(""+bar);
+  else if(ev.keyCode == 87){
+    bar_2.up();
+    //w
+  }
+  else if(ev.keyCode == 83){
+    bar_2.down();
+    //s
+  }
 });
 
-self.addEventListener("load", main);
+
+window.requestAnimationFrame(controller);
 
 // CONTROLADORA QUE INSTANCIA NUESTROS OBJETOS
 
-function main() {
-  console.log("holamunfo")
-  console.log(board);
-  board_view.draw();
+
+function controller() {
+  board_view.play();
+  window.requestAnimationFrame(controller);
+
 }
